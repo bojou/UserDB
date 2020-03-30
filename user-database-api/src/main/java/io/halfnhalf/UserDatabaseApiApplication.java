d5bde024ae4b14037a1081b28ddb2c2a09780dad
Julius <julius.r.hopf@gmail.com>
2020-03-30 13:19:42 +0200

    Update UserDatabaseApiApplication.java

--- user-database-api/src/main/java/io/halfnhalf/UserDatabaseApiApplication.java
+++ user-database-api/src/main/java/io/halfnhalf/UserDatabaseApiApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-
+//Berko är ett fittansikte
 
 @SpringBootApplication
 public class UserDatabaseApiApplication {
