559dab886e4617c1388b033c937cd8572e189e3d
Julius <julius.r.hopf@gmail.com>
2020-03-30 12:05:35 +0200

    Create .project

--- user-database-api/src/main/java/io/halfnhalf/UserDatabaseApiApplication.java
+++ user-database-api/src/main/java/io/halfnhalf/UserDatabaseApiApplication.java
@@ -2,10 +2,11 @@ package io.halfnhalf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+//gasgasga
 
-
-
+@ComponentScan
 @SpringBootApplication
 public class UserDatabaseApiApplication {
 
